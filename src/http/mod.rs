2d53a6e1d8918c33e864e97ecacdd79f12be772d
Péter Leéh <52788117+Ptrskay3@users.noreply.github.com>
2023-09-14 10:29:31 +0200

    Change back port to 8080

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -40,7 +40,7 @@ pub async fn serve(config: Config, db: Database) -> anyhow::Result<()> {
     //
     // Note that any port below 1024 needs superuser privileges to bind on Linux,
     // so 80 isn't usually used as a default for that reason.
-    let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8000));
+    let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8080));
     tracing::debug!(addr=?addr, "Starting server..");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
