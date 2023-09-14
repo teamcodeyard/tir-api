a762f0926bf0df25a5ff550a964275fe0d8b1a06
peter leeh <leeh123peter@gmail.com>
2023-09-14 10:04:52 +0200

    fix deprecation warnings

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,15 @@
 #[derive(clap::Parser)]
 pub struct Config {
     /// The connection URL for the Mongo database this application should use.
-    #[clap(long, env)]
+    #[arg(long, env)]
     pub database_url: String,
- 
-    #[clap(long, env)]
+
+    #[arg(long, env)]
     pub database_name: String,
 
-    #[clap(long, env)]
+    #[arg(long, env)]
     pub roadmap_file_path: String,
-    
-    #[clap(long, env)]
-    pub openai_sk: String,
 
+    #[arg(long, env)]
+    pub openai_sk: String,
 }
