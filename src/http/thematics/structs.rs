338b64109069c780f129940258930fa0fa7d3ac8
Imre Ujlaki <ujlaki.imre@gmail.com>
2023-11-14 07:04:00 +0100

    :sparkles: Add thematic service

--- src/http/thematics/structs.rs
+++ src/http/thematics/structs.rs
@@ -1,6 +1,7 @@
-use super::{DBCollectable, ObjectId};
-use serde::{de, Deserializer};
+use super::{ DBCollectable, ObjectId };
+use serde::{ de, Deserializer };
 use tirengine::Topic;
+use validator::Validate;
 
 impl DBCollectable for Thematic {
     fn get_collection_name() -> &'static str {
@@ -16,10 +17,12 @@ pub struct Thematic {
     pub topics: Vec<Topic>,
 }
 
-fn inline_object_id<'de, D>(deserializer: D) -> Result<String, D::Error>
-where
-    D: Deserializer<'de>,
-{
+#[derive(Debug, serde::Deserialize, serde::Serialize, Validate)]
+pub struct CreateThematicRequest {
+    pub title: String,
+}
+
+fn inline_object_id<'de, D>(deserializer: D) -> Result<String, D::Error> where D: Deserializer<'de> {
     let id: ObjectId = de::Deserialize::deserialize(deserializer)?;
     Ok(id.to_hex())
 }
--- src/http/thematics/thematics.rs
+++ src/http/thematics/thematics.rs
@@ -1,23 +1,50 @@
-use crate::http::ApiContext;
+use super::structs::Thematic;
+use super::structs::CreateThematicRequest;
 use super::ServerError;
+use crate::http::extractors::DatabaseCollection;
+use crate::http::ApiContext;
+use anyhow::Result;
 use axum::routing::get;
 use axum::{ Json, Router };
-use anyhow::Result;
-use mongodb::Database;
-use crate::http::extractors::DatabaseCollection;
 use futures::stream::TryStreamExt;
-use super::structs::Thematic;
+use mongodb::Database;
 
 pub fn router() -> Router<ApiContext> {
-    Router::new().route("/api/thematics", get(list_thematics))
+    Router::new().route("/api/thematics", get(list_thematics).post(add_thematic))
 }
 
 pub async fn create_indexes(_db: &Database) {}
 
-async fn list_thematics(
-    DatabaseCollection(thematic_collection): DatabaseCollection<Thematic>,
-) -> Result<Json<Vec<Thematic>>, ServerError> {
-    let cursor = thematic_collection.find(None, None).await?;
-    let result = cursor.try_collect().await?;
-    Ok(Json(result))
+async fn list_thematics(DatabaseCollection(
+    thematic_collection,
+): DatabaseCollection<Thematic>) -> Result<Json<Vec<Thematic>>, ServerError> {
+    Ok(Json(thematic_collection.find(None, None).await?.try_collect().await?))
+}
+
+#[axum::debug_handler(state = ApiContext)]
+async fn add_thematic(
+    DatabaseCollection(thematic_collection): DatabaseCollection<tirengine::Thematic>,
+    Json(req): Json<CreateThematicRequest>
+) -> Result<Json<Thematic>, ServerError> {
+    let result = thematic_collection.insert_one(
+        tirengine::Thematic {
+            title: req.title.clone(),
+            topics: vec![],
+        },
+        None
+    ).await?;
+
+    Ok(
+        Json(Thematic {
+            _id: result.inserted_id.as_object_id().unwrap().to_hex(),
+            title: req.title,
+            topics: vec![],
+        })
+    )
+}
+
+impl crate::http::thematics::DBCollectable for tirengine::Thematic {
+    fn get_collection_name() -> &'static str {
+        "thematics"
+    }
 }
